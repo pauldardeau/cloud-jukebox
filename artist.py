c2315bdcf9632fb664cf81ac5dea57788ac844f0
Paul Dardeau <pjdardea@pjdardea-MOBL.amr.corp.intel.com>
2015-11-29 12:39:10 -0600

    initial commit of new model objects

---
+++ artist.py
@@ -0,0 +1,5 @@
+class Artist(object):
+    def __init__(self, artist_uid, artist_name, artist_description=None):
+        self.artist_uid = artist_uid
+        self.artist_name = artist_name
+        self.artist_description = artist_description
