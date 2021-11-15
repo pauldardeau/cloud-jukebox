06daad460bc2f32189a064ac317b4b67ffe20db6
Paul Dardeau <pauldardeau@gmail.com>
2021-11-15 08:49:28 -0600

    use python3 for play-artist.sh; show example usage

--- play-artist.sh
+++ play-artist.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 ] ; then
    echo "error: missing artist name"
+   echo 'example: ./play-artist.sh "Van Halen"'
    exit 1
 fi
-python jukebox_main.py --artist="$1" play
+python3 jukebox_main.py --artist="$1" play
