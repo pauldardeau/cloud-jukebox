dc8882f06a18383a7529f604e9c87ed3189059c6
Paul Dardeau <pauldardeau@gmail.com>
2022-01-02 12:02:45 -0600

    add type hint for list ivar songs

--- playlist.py
+++ playlist.py
@@ -1,6 +1,11 @@
+from typing import List
+
+import song_metadata
+
+
 class Playlist(object):
     def __init__(self, playlist_uid: str, playlist_name: str, playlist_description: str = ""):
         self.playlist_uid: str = playlist_uid
         self.playlist_name: str = playlist_name
         self.playlist_description: str = playlist_description
-        self.songs = []
+        self.songs: List[song_metadata.SongMetadata] = []
