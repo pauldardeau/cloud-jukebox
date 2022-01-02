f88869846ea6406527cc5b280fd64733043a7090
Paul Dardeau <pauldardeau@gmail.com>
2022-01-02 11:56:56 -0600

    remove 'mode' argument name for open command

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import hashlib
 
 
 def md5_for_file(path_to_file: str) -> str:
-    with open(path_to_file, mode='rb') as f:
+    with open(path_to_file, 'rb') as f:
         d = hashlib.md5()
         for buf in f.read(4096):
             d.update(buf)
