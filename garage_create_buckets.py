ad6d7d07ac7b05186f8a1c01861d5e2cbf20bebc
Paul Dardeau <775266+pauldardeau@users.noreply.github.com>
2025-08-12 09:47:21 -0500

    create additional metadata buckets

--- garage_create_buckets.py
+++ garage_create_buckets.py
@@ -1,4 +1,16 @@
 import os
+import subprocess
+
+
+def run_command(command):
+    p = subprocess.run(command,
+                       shell=True,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+    exit_code = p.returncode
+    stdout = p.stdout.decode("utf-8")
+    stderr = p.stderr.decode("utf-8")
+    return exit_code, stdout, stderr
 
 
 def run_system_command(cmd):
@@ -15,16 +27,27 @@ def grant_access_to_bucket_for_key(bucket_name, garage_app_key):
 
 def create_bucket(bucket_name):
     cmd = "garage bucket create %s" % bucket_name
-    run_system_command(cmd)
+    rc, _, _ = run_command(cmd)
+    return rc == 0
+
+
+def bucket_exists(bucket_name):
+    cmd = "garage bucket info %s" % bucket_name
+    rc, _, _ = run_command(cmd)
+    return rc == 0
 
 
 def main(garage_app_key):
     bucket_names = []
+    created_buckets = []
 
     letters = 'abcdefghijklmnopqrstuvwxyz'
     digits = '0123456789'
 
     bucket_names.append('music-metadata')
+    bucket_names.append('albums')
+    bucket_names.append('album-art')
+    bucket_names.append('playlists')
 
     for letter in letters:
         bucket_name = "%s-artist-songs" % letter
@@ -35,9 +58,11 @@ def main(garage_app_key):
         bucket_names.append(bucket_name)
 
     for bucket_name in bucket_names:
-        create_bucket(bucket_name)
+        if not bucket_exists(bucket_name):
+            if create_bucket(bucket_name):
+                created_buckets.append(bucket_name)
 
-    for bucket_name in bucket_names:
+    for bucket_name in created_buckets:
         grant_access_to_bucket_for_key(bucket_name, garage_app_key)
 
 
